8dcb41b418aad18f2f9bf889a46bb13dfc7cf5ff
rtmonteiro <ryan.monteiro@aluno.ufes.br>
2023-11-22 14:58:31 -0300

    feat: starts login page

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginModule } from './pages/login/login.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "*",
+    component: LoginComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/angular/src/app/pages/login/login.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { LoginComponent } from "./login.component";
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    LoginComponent
+  ]
+})
+export class LoginModule {}

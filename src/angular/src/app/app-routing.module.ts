4d86d36f6aa16e5ca3f1da48d2812ee184f67d97
pedrovic7997 <pedrovictor6974@gmail.com>
2023-11-29 14:11:52 -0300

    hotfix: import login component

--- src/angular/src/app/app-routing.module.ts
+++ src/angular/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginModule } from './pages/login/login.module';
+import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
   {

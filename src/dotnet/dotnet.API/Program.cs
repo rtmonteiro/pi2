f0b22ccdde1c601ca687e7185e112be85e86de0f
Rogério Medeiros <rogerio.rmsj@gmail.com>
2023-12-06 00:21:29 -0300

    feat: AuthenticationSetup

--- src/dotnet/dotnet.API/Program.cs
+++ src/dotnet/dotnet.API/Program.cs
@@ -10,7 +10,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSwagger();
